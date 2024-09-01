ac9d3e407bf7beac9877ce3054ae70f0cca72d79
deepak1051 <deepakpanwar1051@gmail.com>
2024-09-02 02:11:43 +0530

    build script added

--- index.js
+++ index.js
@@ -15,5 +15,19 @@ app.use(express.json());
 
 app.use('/api/snippets', snippetRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`running on port ${port}`));
