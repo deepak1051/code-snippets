cff5f345de7d2876ef1d22fe563e332c9cda3bae
deepak1051 <deepakpanwar1051@gmail.com>
2025-02-12 22:06:02 +0530

    create category image upload

--- src/api/category/category.ct.js
+++ src/api/category/category.ct.js
@@ -1,5 +1,12 @@
+import path from 'node:path';
 import Category from '../../models/Category.js';
 import Snippet from '../../models/Snippet.js';
+
+import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 export const getAllCategory = async (req, res) => {
   try {
@@ -18,18 +25,28 @@ export const createCategory = async (req, res) => {
       return res.status(400).json({ message: 'name is required' });
     }
 
+    const files = req.files;
+
+    const filePath = files?.image?.[0]?.path;
+
     const categoryExist = await Category.findOne({ name });
 
     if (categoryExist) {
+      fs.unlinkSync(filePath);
+
       return res.status(400).json({ message: 'category already exist' });
     }
 
     const category = await Category.create({
       name,
+      image: filePath,
     });
 
     return res.status(201).json(category);
   } catch (error) {
+    if (req.files?.image?.[0]?.path) {
+      fs.unlinkSync(req.files?.image?.[0]?.path);
+    }
     return res.status(500).json({ message: 'something went wrong' });
   }
 };
--- src/api/category/category.route.js
+++ src/api/category/category.route.js
@@ -1,18 +1,52 @@
 import express from 'express';
+import path from 'node:path';
 import { auth } from '../../middleware/auth.middleware.js';
-
+import fs from 'node:fs';
 import {
   createCategory,
   getAllCategory,
   getCategorySnippet,
 } from './category.ct.js';
+import multer from 'multer';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const router = express.Router();
+const uploadDir = path.join(process.cwd(), '/public/data/uploads');
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fieldSize: 3e7 }, //30mb
+});
 
 router.get('/', getAllCategory);
 
 router.get('/:categoryId', getCategorySnippet);
 
-router.post('/', auth, createCategory);
+router.post(
+  '/',
+  // auth,
+  upload.fields([{ name: 'image', maxCount: 1 }]),
+  // (req, res) => {
+  //   res.send('It Worked');
+  // }
+  createCategory
+);
 
 export default router;
